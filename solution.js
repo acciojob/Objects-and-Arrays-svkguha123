f18f0209cfb117af6fff68048efa6d7e225dfc64
Souvik Guha <svkguha123@gmail.com>
2023-04-01 21:09:06 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -7,5 +7,8 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+const team = players;
+const team1 = [...players];
+const cap1 = {...person};
 
 export {players,person,team,team1,cap1}
